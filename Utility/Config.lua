1b96e14f87f5c6e2ef2b319dc200e806833502f3
Steyer Caans Herzog <kessie@nyx.to>
2022-04-21 13:46:10 +0100

    Create Config.lua

---
+++ Utility/Config.lua
@@ -0,0 +1,36 @@
+--{{{ Dependencies
+local Nyx = require "gamesense/Nyx/v1/Api/Nyx"
+local Table = require "gamesense/Nyx/v1/Api/Table"
+--}}}
+
+--{{{ Config
+--- @class Config : Class
+--- @field administrators string[]
+--- @field isDebugging boolean
+--- @field isLiveClient boolean
+--- @field isEmulatingRealUserInput boolean
+--- @field openAiApiKey string
+local Config = {
+    administrators = {
+        -- Add SteamID64 of accounts you wish to have as administrators.
+        -- These accounts can use extra chat commands.
+    },
+    isDebugging = false, -- Enables debugging features.
+    isLiveClient = false, -- Enable this when running on the Dominion Service.
+    isEmulatingRealUserInput = false, -- Enable this to emulate mouse-keyboard. Results in less accurate movement. Avoids potential bot detection.
+    openAiApiKey = "" -- Set this to provide an API key for use with the Open AI chatbot.
+}
+
+--- @return void
+function Config:__setup()
+    Config.administrators = Table.getMap(Config.administrators)
+end
+
+--- @param steamId64 string
+--- @return boolean
+function Config.isAdministrator(steamId64)
+    return Config.administrators[steamId64]
+end
+
+return Nyx.class("Config", Config)
+--}}}
