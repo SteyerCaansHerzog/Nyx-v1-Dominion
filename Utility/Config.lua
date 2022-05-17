25c2e86f9d7c0e0b9f457e2dbd86649acc991c43
Steyer Caans Herzog <kessie@nyx.to>
2022-05-17 10:43:48 +0100

    Update Config.lua

--- Utility/Config.lua
+++ Utility/Config.lua
@@ -6,24 +6,26 @@ local Table = require "gamesense/Nyx/v1/Api/Table"
 --{{{ Config
 --- @class Config : Class
 --- @field administrators string[]
---- @field isDebugging boolean
---- @field isLiveClient boolean
---- @field isEmulatingRealUserInput boolean
---- @field openAiApiKey string
 --- @field clientFocusVolume number
 --- @field defaultSkillLevel number
 --- @field isClearingSteamFriends boolean
+--- @field isDebugging boolean
+--- @field isEmulatingRealUserInput boolean
+--- @field isLiveClient boolean
+--- @field isRandomizingCrosshair boolean
+--- @field openAiApiKey string
 local Config = {
     administrators = {
         -- Insert SteamID64s here that should be administrative accounts.
     },
-    isDebugging = false, -- Enables debugging features.
-    isLiveClient = false, -- Enable this when running on the Dominion Service.
-    isEmulatingRealUserInput = false, -- Enable this to emulate mouse-keyboard. Results in less accurate movement. Avoids potential bot detection.
-    openAiApiKey = "", -- Set this to provide an API key for use with the Open AI chatbot.
     clientFocusVolume = 1.0, -- The volume of a Reaper client that is focused.
     defaultSkillLevel = 4, -- The skill level to set the AI to by default.
-    isClearingSteamFriends = false -- Clear the AI's Steam friend list and any requests.
+    isClearingSteamFriends = false, -- Clear the AI's Steam friend list and any requests.
+    isDebugging = false, -- Enables debugging features.
+    isEmulatingRealUserInput = false, -- Enable this to emulate mouse-keyboard. Results in less accurate movement. Avoids potential bot detection.
+    isLiveClient = false, -- Enable this when running on the Dominion Service.
+    isRandomizingCrosshair = false, -- Create random crosshair every time Dominion is initialised.
+    openAiApiKey = "", -- Set this to provide an API key for use with the Open AI chatbot.
 }
 
 --- @return void
