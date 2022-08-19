599cf25de09245eee54aa94f6b6c130660ed9bc9
Steyer Caans Herzog <kessie@nyx.to>
2022-08-19 16:33:38 +0100

    Update Config.lua

--- Utility/Config.lua
+++ Utility/Config.lua
@@ -21,22 +21,7 @@ local Table = require "gamesense/Nyx/v1/Api/Table"
 --- @field virtualMouseMode string ridid | dynamic
 local Config = {
     administrators = {
-        -- Friends
-        "76561198138748363", -- Adrian
-        "76561198080048177", -- Fanta
-        -- Main accounts
-        "76561198339559079", -- Data
-        "76561198373386496", -- Bropp
-        "76561198816968549", -- Kirsty
-        "76561198866118626", -- Michael
-        "76561198283352893", -- Comm banned
-        "76561199241945029", -- Digital Spring
-        -- Others
-        --"76561198887876627", -- Phil
-        --"76561198849675001", -- Rittenhouse
-        --"76561198994143335", -- Hoss
-        --"76561198391892203", -- Smithz
-        "76561198326826469" -- Lobenzo
+        "76561198138748363", -- List of SteamIDs that are allowed to run admin-only chat commands.
     },
     clientConfigs = {
         normal = "Nyx-v1-Dominion", -- GS config to load when in normal mode.
@@ -44,14 +29,14 @@ local Config = {
     },
     clientFocusVolume = 0.15, -- The volume of a Reaper client that is focused.
     defaultSkillLevel = 4, -- The skill level to set the AI to by default.
-    isClearingSteamFriends = true, -- Clear the AI's Steam friend list and any requests.
+    isClearingSteamFriends = false, -- Clear the AI's Steam friend list and any requests.
     isDebugging = false, -- Enables debugging features.
     isEmulatingRealUserInput = true, -- Enable this to emulate mouse-keyboard. Results in less accurate movement. Avoids potential bot detection.
     isLiveClient = false, -- Enable this when running on the Dominion Service.
     isRandomizingCrosshair = false, -- Create random crosshair every time Dominion is initialised.
     isTextModeAllowed = true, -- Enable this to disable Source engine rendering when applicable.
     language = "English", -- Language localization for logs and other text.
-    openAiApiKey = "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", -- Set this to provide an API key for use with the Open AI chatbot.
+    openAiApiKey = "XXXX", -- Set this to provide an API key for use with the Open AI chatbot.
     projectDirectory = "lua/gamesense/Nyx/v1/Dominion/%s", -- Root project directory for the Dominion folder. Must have /%s at the end.
     virtualMouseMode = "dynamic" -- "rigid" for old method, "dynamic" for smoother new method.
 }
