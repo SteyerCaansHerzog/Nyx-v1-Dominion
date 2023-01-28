a592e6cb0a15f6cadf0f012b3e7e0940b79a10ab
Steyer Caans Herzog <kessie@nyx.to>
2023-01-28 18:45:47 +0000

    Update Dominion.lua

--- Dominion.lua
+++ Dominion.lua
@@ -7,6 +7,6 @@
 --- - Localization-API <https://gamesense.pub/forums/viewtopic.php?id=30643>
 --- - Web-Sockets-API <https://gamesense.pub/forums/viewtopic.php?id=23653>
 
-require "gamesense/Nyx/v1/Dominion/Utility/Logger".credits("2.4.6", "19/01/2022")
+require "gamesense/Nyx/v1/Dominion/Utility/Logger".credits("2.5.0", "21/01/2023")
 require "gamesense/Nyx/v1/Dominion/Ai/Ai":new()
 require "gamesense/Nyx/v1/Dominion/Traversal/NodegraphEditor":new()
